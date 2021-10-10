748a7755a625116bb70db475b1c10555654b048c
Jhair-andree <66096069+Jhair-andree@users.noreply.github.com>
2021-10-09 20:24:22 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <link rel="stylesheet" href="styles.css">
+      <link rel="javascript" href="JavaScript.js">
+      <body onload="LoadingPage ()">
+        <style>
+          div.a {
+            text-align: center;
+          }
+          </style>
+          <img src="https://cutt.ly/Ez5oWhf" height="848 dp" width="1500 dp">
+          <img src="https://cutt.ly/pz5ozda" height="1200 dp" width="1500 dp">
+          <div class="a">
+        <h1 size="100">
+        <h1 onmouseover="style.color='blue'" onmouseout="style.color='yellow'" onmouseup="style.color='red'"onmousedown="style.color='green'" >EXPORTA Perú</h1>
+        <style>
+          div.a {
+            text-align: center;
+          }
+          </style>
+          
+          <div class="a">
+          <h1>Iniciar sesión</h1>
+          <p>Todos nuestros canales de atención son 100% digitales - EXPORTA Perú.</p>
+          </div>
+        </br>
+      </br>
+      </br>
+      <style>
+        div.a {
+          text-align: center;
+        }
+        </style>
+        
+        <div class="a">
+        </div>
+          </br>
+          </br>
+          <style>
+            div.a {
+              text-align: center;
+            }
+            </style>
+            
+            <div class="a">
+            <label>Enter your username:</label>
+            <input type="text" onfocus="myFunction(this)" onfocus="access(this)">
+            </div>
+      <style>
+        div.a {
+          text-align: center;
+        }
+        </style>
+        
+        <div class="a">
+        <label>Enter your password:</label>
+        <input type="text" onfocus="myFunction(this)" onfocus="access(this)">
+        </div>      
+    </br>
+  </br>
+</br>
+<style>
+  div.a {
+    text-align: center;
+  }
+  </style>
+  
+  <div class="a">
+  <label>¿Enviar sugerencias?</label>
+  <input type="text" onfocus="myFunction(this)">
+  </div>  
+  <style>
+    div.a {
+      text-align: center;
+    }
+    </style>
+    <div class="a">    
+<form onsubmit="SubmitEvent()" action="https://www.facebook.com/cre.ser2020/?view_public_for=564806343983820">
+</br>
+</br>
+</br>
+  <input type="submit">
+</form>
+<form onreset="message()"></form>
+</br>
+</br>
+      </h1>
+      </div>
+            <form>
+                <input type="button" value="Iniciar sesión" onclick="OpenNewWindow()">
+              </form>
+              <form onreset="message()" action="https://creserkidsperu.neocities.org/">
+                </form>
+              <form>
+                <input type="button" value="VERLU Jhairand Aliments" onclick="OpenNewWindow()">
+              </form>
+              <form onreset="message()" action="https://creserkidsperu.neocities.org/">
+                </form>
+              <form>
+                <input type="button" value="Realiza tu pedido" onclick="OpenNewWindow()">
+              </form>
+            <form onreset="message()" action="https://creserkidsperu.neocities.org/">
+            </form>
+            <form>
+              <input type="button" value="Configuraciones" onclick="OpenNewWindow()">
+            </form>
+            <form onreset="message()" action="https://creserkidsperu.neocities.org/">
+              </form>
+              <style>
+                div.a {
+                  text-align: center;
+                }
+                </style>
+                
+                <div class="a">
+              <h1 onmouseover="style.color='blue'" onmouseout="style.color='yellow'" onmouseup="style.color='red'"onmousedown="style.color='green'" >Somos una empresa agroexportadora de alimentos </h1>
+            <h2>_____________________________________________________________________________________________________________________________</h2>
+            </div>
+            <img src="https://www.facebook.com/cre.ser2020/photos/a.564806400650481/1064619404002509/">
+            <div>
+                <ul>
+                      <form onsubmit="SubmitEvent()" action="https://www.facebook.com/cre.ser2020/?view_public_for=564806343983820">
+                      </br>
+                      </br>
+                      </br>
+                        <input type="submit" value="Descarga EXPORTA Perú para tu PC">
+                      </form>
+                      <form onreset="message()"></form>
+                    <br/>
+                    <br/>
+                      <form onsubmit="SubmitEvent()" action="https://www.facebook.com/cre.ser2020/?view_public_for=564806343983820">
+                      </br>
+                      </br>
+                      </br>
+                        <input type="submit" value="Registrarse en línea para recibir una charla virtual con nuestros asesores">
+                      </form>
+                      <form onreset="message()"></form>
+                    <br/>
+                    <br/>
+                      <form onsubmit="SubmitEvent()" action="https://www.facebook.com/cre.ser2020/?view_public_for=564806343983820">
+                      </br>
+                      </br>
+                      </br>
+                        <input type="submit" value="Pedir informes en línea">
+                      </form>
+                      <form onreset="message()"></form>
+                    <br/>
+                    <br/>
+                </ul>
+            </div>
+            <img src="https://www.facebook.com/cre.ser2020/photos/a.564808870650234/1065811273883322"> 
+          </br>
+        </br>
+        </br>
+<style>
+  div.a {
+    text-align: center;
+  }
+  </style>
+  
+  <div class="a">
+  <h2>+-+-+-+-+-+-+-+-+-REGISTRATION LOG UP AREA-+-+-+-+-+-+-+-+-+</h2>
+  <size="80" onfocus="myFunction(this)">
+  </div>         
+  </br>
+            </br>
+          </br>
+          <style>
+            div.a {
+              text-align: center;
+            }
+            </style>
+            
+            <div class="a">
+            <label>Enter your first names:</label>
+            <input type="text" size="10" onfocus="myFunction(this)">
+            </div>      
+            </br>
+            <style>
+              div.a {
+                text-align: center;
+              }
+              </style>
+              
+              <div class="a">
+              <label>Enter your last names:</label>
+              <input type="text" size="10" onfocus="myFunction(this)">
+              </div>      
+            </br>
+            <style>
+              div.a {
+                text-align: center;
+              }
+              </style>
+              
+              <div class="a">
+              <label>Enter your nickname:</label>
+              <input type="text" size="10" onfocus="myFunction(this)">
+              </div>    
+            </br>
+            <style>
+              div.a {
+                text-align: center;
+              }
+              </style>
+              <div class="a">
+                <input type="submit" value="REGISTRAR USUARIO NUEVO">
+              </br>
+            </br>
+            </br>
+            <input type="reset" value="Cancelar">
+          </div>
+          </br>
+            </br>
+            </br>
+              </form>
+            </br>
+          </br>
+          <input type="text" value="" height="auto" width="450">
+          <form>
+            <input type="button" value="CONSULTA aquí" onclick="OpenNewWindow()">
+          </form>
+              <h1 onmouseover="style.color='skyblue'" onmouseout="style.color='yellow'">Venta de vegetales frutas y hortalizas para exportación</h1>
+              <form>
+                <input type="button" value="Cotiza aquí" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+              </br>
+            </br>
+                <img src="https://cutt.ly/txebUKq" height="400" width="900">
+              </br>
+            </br>
+                <input type="button" value="Haz una cita o videoconferencia con un asesor " onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+              </br>
+            </br>
+            <input type="button" value="Realiza tu pedido aquí" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+          </br>
+        </br>
+            <img src="https://cutt.ly/8xevfAV" height="400" width="900">
+          </br>
+        </br>
+            <input type="button" value="Exporta aquí" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+              </form>
+              <h1 onmouseover="style.color='green'" onmouseout="style.color='orange'">VERLU Jhairand Aliments</h1>
+            </br>
+          </br>
+              <form>
+                <input type="button" value="Deli Pasta" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+                <input type="button" value="Omega Detergentes" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+              </br>
+            </br>
+            <input type="button" value="Hadey Blanqueadores" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+                <input type="button" value="Hiley Lava vajillas & Jabones de tocador y baño" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+              </form>
+              <h1 onmouseover="style.color='red'" onmouseout="style.color='blue'">JAVELU Aliments</h1>
+              <form>
+                <input type="button" value="Quinua Cola" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+                <input type="button" value="Xaek Desodorantes" onclick="OpenNewWindow()" action="https://creserkidsperu.neocities.org/">
+              </br>
+            </br>
+            <input type="button" value="Arroz del campo" onclick="OpenNewWindow()"action="https://creserkidsperu.neocities.org/">
+                <input type="button" value="Papel higienico El Inca" onclick="OpenNewWindow()"action="https://creserkidsperu.neocities.org/">
+                <input type="button" value="Papel toalla El Inca" onclick="OpenNewWindow()"action="https://creserkidsperu.neocities.org/">
+              </form>
+            </br>
+          </br>
+        </br>
+              <aside>
+            <label> VERLU Jhairand | SITE PLATFORM ADVERTISEMENT</label>
+            <h2>VERLU Jhairand Aliments</h2>
+            <input type="text" value="" height="auto" width="450">
+          <form>
+            <input type="button" value="CONSULTAS aquí" onclick="OpenNewWindow()">
+          </form>
+          </br>
+        </br>
+      </br>
+            <h2>VERILÚ Corp</h2>
+          </br>
+        </br>
+      </br>
+      <iimg resource="VERLU Jhairand Aliments.png">
+    </br>
+  </br>
+</br>
+            <h2>Agrónomos del Perú</h2>
+          </br>
+        </br>
+      </br>
+            <img src="">
+          </br>
+        </br>
+      </br>
+    </br>
+  </br>
+</br>
+      <h2>SUPER Vea Supermercado online</h2>
+    </br>
+  </br>
+</br>
+      <img src="">
+    </br>
+  </br>
+</br>
+</br>
+</br>
+</br>
+    <h2>Veralú Alliance Peru-Francia</h2>
+  </br>
+</br>
+</br>
+<img src="">
+  </br>
+</br>
+</br>
+    </aside>
+  </body>
+    </head>
+  </br>
+</br>
+</br>
+    <footer>
+<label>Déjanos saber sobre tu experiencia laboral y adjunta tu CV. Califica a diversos puestos de empleo</label>
+<input type="file" text-align="center" value="Subir tu CV curriculum vitae">
+<input type="submit" text-align="center" value="Enviar tu CV">
+</br>
+</br>
+</br>
+      <textarea></textarea>
+    </br>
+  </br>
+  </br>
+<p onmousedown="ActionMouse(this,'green')" onmouseup="ActionMouse(this,'orange')">
+Aceptar cookies y almacenamiento de datos de terceros en tu dispositivo.
+ </p>
+ <button onclick="Button()">ACCEPT COOKIES AND CACHE</button>
+ <p id="action"></p>
+ <style>
+  div.a {
+    text-align: center;
+  }
+  </style>
+  
+  <div class="a">
+  <img src="https://acortar.link/4c4ZW" id="https://creserkidsperu.neocities.org/asset/1" height="160" width="90" class="button" type="text" label="libro_reclamo">
+  </br>
+</br>
+</br>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<button class="btn btn-success">¿Aún deseas conocer más de nosotros?</button>
+</br>
+</br>
+<a href="#" class="btn btn-success">¿Conoce todos los beneficios que tenemos para ti. Si estas buscando trabajo, y deseas postular? Haz click aquí para enviar tu CV</a>
+<h6>©2021 Todos los derechos reservados - VerLu Ltd Co Jhairand | EXPORTA Perú - Produced by ADESOFT Studios</h6>
+  <p>Libro de reclamaciones digital - EXPORTA Perú.</p>
+</div>
+</footer>
+</html>
